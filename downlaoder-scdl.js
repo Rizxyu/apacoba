ba28c7029adf69a53f66077813c34f0024c098a5
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-07-17 08:10:58 +0700

    Update downlaoder-scdl.js

--- downlaoder-scdl.js
+++ downlaoder-scdl.js
@@ -1,19 +1,38 @@
-import fetch from 'node-fetch'
+import { Spotify } from "spotifydl-core"
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-let cao = await fetch(APIs.skyz + '/downloader/scdl?link=' + args[0])
-let json = await cao.json()
-let res = {
-creator: 'ANJAY YAHAHAHHAHA HAYU SLEBEEEEWWWWWWWWWWSS SUBREK RIZXYU COYYY FOLLOW IG: rizxyux',
-audio: json.link,
-img: json.thumb,
-judul: json.title
+
+const spotify = new Spotify({
+    clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
+    clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009'
+})
+
+
+let kp = await spotify.getTrack("https://open.spotify.com/track/2lCPDg2EWErPYdO3ItPUdH?si=G8L5QQOxQmiJgfhdYBLOuw&utm_source=copy-link&context=spotify%3Aplaylist%3A37i9dQZF1DWWY64wDtewQt")
+
+let kp2 = await spotify.downloadTrack("https://open.spotify.com/track/2lCPDg2EWErPYdO3ItPUdH?si=G8L5QQOxQmiJgfhdYBLOuw&utm_source=copy-link&context=spotify%3Aplaylist%3A37i9dQZF1DWWY64wDtewQt")
+
+var res = {
+creator: "Follow IG: rizxyux",
+judul: kp.name,
+artis: kp.artists,
+album: kp.album_name,
+rilis: kp.realese_date,
+thumb: kp.cover_url,
+audio: kp2
 }
-m.reply(wait)
+
 console.log(res)
-conn.sendFile(m.chat, res.img, 'scdl.jpg', `*SOUNDCLOUD DL*\n*Judul:* ${res.judul}\n*Link:*${res.link}`
-, m)
-conn.sendFile(m.chat, res.audio, res.judul + '.mp3', null, m)
+m.reply(wait)
+conn.sendFile(m.chat, res.thumb, res.judul +'.jpg', `*SPOTIFY DOWNLOADER*
+\n*Judul:* ${res.judul}
+*Artis:* ${res.artis}
+*Album:* ${res.album}
+*Dipublish:* ${res.rilis}
+
+Support Me https://trakteer.id/rizxyugnxng`, m)
+
+await conn.sendFile(m.chat, res.audio, res.judul + ".mp3", null, m, null, { asDocument: true})
 }
 handler.help = ['soundcloud'].map(v => v + ' <url>')
 handler.tags = ['downloader']
